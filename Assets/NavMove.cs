4188099c262ef9b66befa48820026da75fdd60fd
hktrollgames <hktrollgames@gmail.com>
2021-05-31 18:55:25 +0900

    네비메시 에이전트가 지정한 트렌스폼 따라가게함

---
+++ Assets/NavMove.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.AI;
+
+public class NavMove : MonoBehaviour
+{
+    public NavMeshAgent agent;
+    public Transform follower;
+
+    void Start()
+    {
+        agent = GetComponent<NavMeshAgent>();
+    }
+
+    void Update()
+    {
+        agent.destination = follower.position;
+    }
+}
