ad390baf612b0fc0f1acf791591e38f305ad0363
hktrollgames <hktrollgames@gmail.com>
2021-06-04 20:37:51 +0900

    Linq샘플

--- Assets/Scripts/Editor/MyEditorMenu.cs
+++ Assets/Scripts/Editor/MyEditorMenu.cs
@@ -1,5 +1,6 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
+using System.Linq;
 using UnityEditor;
 using UnityEngine;
 
@@ -43,7 +44,8 @@ public class MyEditorMenu : ScriptableWizard
         // 메테리얼의 설정을 모두 복사하자
         Debug.Assert(Selection.objects.Length == 1, $"선택된 오브젝트의 개숫가 {Selection.objects.Length}개 입니다");
         Renderer renderer = Selection.activeGameObject.GetComponent<Renderer>();
-        Material toMat = renderer.material;
+        //Material toMat = renderer.material;
+        Material toMat = renderer.sharedMaterial;
         Shader toShader = toMat.shader;
 
         int count = ShaderUtil.GetPropertyCount(toShader);
@@ -80,9 +82,10 @@ public class MyEditorMenu : ScriptableWizard
     private List<ShaderPropertyInfo> properties;
     internal class ShaderPropertyInfo
     {
-        public string name;
-        public ShaderUtil.ShaderPropertyType type;
-        public dynamic value;
+        public string name; // 쉐이더에서 값 읽음
+        public ShaderUtil.ShaderPropertyType type;// 쉐이더에서 값 읽음
+
+        public dynamic value; // 메테리얼에 있는 값 읽기, 이 값을 다른 메테리얼에 붙여넣음.
 
         public ShaderPropertyInfo(string name, ShaderUtil.ShaderPropertyType type)
         {
@@ -92,14 +95,7 @@ public class MyEditorMenu : ScriptableWizard
     }
     private void PasteShaderConfig()
     {
-        // 선택한 모든 오브젝트에 미리 복사했던 설정을 붙여넣자.
-        List<Material> desMaterials = new List<Material>();
-
-        foreach (var item in Selection.objects)
-        {
-            desMaterials.Add(((GameObject)item).GetComponent<Renderer>().sharedMaterial);
-        }
-
+        List<Material> desMaterials = GetSelectedMaterials();
 
         foreach (var desMat in desMaterials)
         {
@@ -119,14 +115,38 @@ public class MyEditorMenu : ScriptableWizard
                     case ShaderUtil.ShaderPropertyType.Range:
                         desMat.SetFloatArray(item.name, item.value); // 테스트 안해봤음.
                         break;
-                    //case ShaderUtil.ShaderPropertyType.TexEnv:
-                    //    desMat.SetTexture(item.name, item.textureValue);
-                    //    break;
+                        //case ShaderUtil.ShaderPropertyType.TexEnv:
+                        //    desMat.SetTexture(item.name, item.textureValue);
+                        //    break;
                 }
             }
         }
     }
 
+    private static List<Material> GetSelectedMaterials()
+    {
+        List<Material> desMaterials = new List<Material>();
+        foreach (var item in Selection.objects)
+        {
+            if (item == null)
+                continue;
+            System.Type itemType = item.GetType();
+            if (itemType == typeof(Material))
+                desMaterials.Add((Material)item);
+            else if (itemType == typeof(GameObject))
+            {
+                //var materials = ((GameObject)item).GetComponentsInChildren<Renderer>().Select(x => x.sharedMaterial);
+                //desMaterials.AddRange(materials);
+
+                var renderers = ((GameObject)item).GetComponentsInChildren<Renderer>();
+                for (int i = 0; i < renderers.Length; i++)
+                {
+                    desMaterials.Add( renderers[i].sharedMaterial);
+                }
+            }
+        }
+        return desMaterials;
+    }
 
     private void ChangeSelctedShader()
     {
