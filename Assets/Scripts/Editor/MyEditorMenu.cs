dabea35f95063b0e14242154cb7ee931acd24169
hktrollgames <hktrollgames@gmail.com>
2021-06-04 19:54:55 +0900

    쉐이더 설정 복사 기능 다시 구현

--- Assets/Scripts/Editor/MyEditorMenu.cs
+++ Assets/Scripts/Editor/MyEditorMenu.cs
@@ -24,8 +24,109 @@ public class MyEditorMenu : ScriptableWizard
             ChangeSelctedShader();
         }
 
+        if (GUILayout.Button("설정 복사"))
+        {
+            CopyShaderConfig();
+        }
+
+        if (GUILayout.Button("설정 붙여넣기"))
+        {
+            PasteShaderConfig();
+        }
+
         return true;
     }
+
+    private void CopyShaderConfig()
+    {
+        // 내가 지금 선택한 오브젝트에 있는 메테리얼에 접근,
+        // 메테리얼의 설정을 모두 복사하자
+        Debug.Assert(Selection.objects.Length == 1, $"선택된 오브젝트의 개숫가 {Selection.objects.Length}개 입니다");
+        Renderer renderer = Selection.activeGameObject.GetComponent<Renderer>();
+        Material toMat = renderer.material;
+        Shader toShader = toMat.shader;
+
+        int count = ShaderUtil.GetPropertyCount(toShader);
+        properties = new List<ShaderPropertyInfo>(count);
+        for (var i = 0; i < count; ++i)
+        {
+            var name = ShaderUtil.GetPropertyName(toShader, i);
+            var type = ShaderUtil.GetPropertyType(toShader, i);
+            properties.Add(new ShaderPropertyInfo(name, type));
+        }
+
+        foreach (var item in properties)
+        {
+            switch (item.type)
+            {
+                case ShaderUtil.ShaderPropertyType.Color:
+                    item.value = toMat.GetColor(item.name);
+                    break;
+                case ShaderUtil.ShaderPropertyType.Vector:
+                    item.value = toMat.GetVector(item.name);
+                    break;
+                case ShaderUtil.ShaderPropertyType.Float:
+                    item.value = toMat.GetFloat(item.name);
+                    break;
+                case ShaderUtil.ShaderPropertyType.Range:
+                    toMat.GetFloatArray(item.name, item.value);
+                    break;
+                    //case ShaderUtil.ShaderPropertyType.TexEnv: // 텍스쳐는 복사할 필요 없으므로 제외
+                    //    item.textureValue = toMat.GetTexture(item.name);
+                    //break;
+            }
+        }
+    }
+    private List<ShaderPropertyInfo> properties;
+    internal class ShaderPropertyInfo
+    {
+        public string name;
+        public ShaderUtil.ShaderPropertyType type;
+        public dynamic value;
+
+        public ShaderPropertyInfo(string name, ShaderUtil.ShaderPropertyType type)
+        {
+            this.name = name;
+            this.type = type;
+        }
+    }
+    private void PasteShaderConfig()
+    {
+        // 선택한 모든 오브젝트에 미리 복사했던 설정을 붙여넣자.
+        List<Material> desMaterials = new List<Material>();
+
+        foreach (var item in Selection.objects)
+        {
+            desMaterials.Add(((GameObject)item).GetComponent<Renderer>().sharedMaterial);
+        }
+
+
+        foreach (var desMat in desMaterials)
+        {
+            foreach (var item in properties)
+            {
+                switch (item.type)
+                {
+                    case ShaderUtil.ShaderPropertyType.Color:
+                        desMat.SetColor(item.name, item.value);
+                        break;
+                    case ShaderUtil.ShaderPropertyType.Vector:
+                        desMat.SetVector(item.name, item.value);
+                        break;
+                    case ShaderUtil.ShaderPropertyType.Float:
+                        desMat.SetFloat(item.name, item.value);
+                        break;
+                    case ShaderUtil.ShaderPropertyType.Range:
+                        desMat.SetFloatArray(item.name, item.value); // 테스트 안해봤음.
+                        break;
+                    //case ShaderUtil.ShaderPropertyType.TexEnv:
+                    //    desMat.SetTexture(item.name, item.textureValue);
+                    //    break;
+                }
+            }
+        }
+    }
+
 
     private void ChangeSelctedShader()
     {
