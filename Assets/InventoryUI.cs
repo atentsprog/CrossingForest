ee472b97a2f96173db3b9ab93fad6bae5acabfe4
hktrollgames <hktrollgames@gmail.com>
2021-06-09 08:53:19 +0900

    인벤토리 아이템 배치 작업중

--- Assets/InventoryUI.cs
+++ Assets/InventoryUI.cs
@@ -1,13 +1,21 @@
-using System.Collections;
+﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
 public class InventoryUI : MonoBehaviour
 {
-    // Start is called before the first frame update
+    public GameObject posItem;
+    public Sprite[] itemList;
     void Start()
     {
-        
+        // 10개를 배치하자.
+        int maxWidthCount = 10;
+        for (int i = 0; i < maxWidthCount; i++)
+        {
+            Animator animator = (Instantiate(posItem, transform)).GetComponent<Animator>();
+            float pos = (float)i / maxWidthCount;
+            animator.Play("InventoryPos", 0, pos);
+        }
     }
 
     // Update is called once per frame
